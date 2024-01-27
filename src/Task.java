fa93854aebf0093ec9b87f4f3cb8ff94e9c5a6e8
Ruben Sahakyan <sahakyanruben1981@gmail.com>
2024-01-27 15:13:33 +0100

    add deadLine field Task class

--- src/Task.java
+++ src/Task.java
@@ -1,17 +1,24 @@
+import java.time.LocalDate;
+
 public class Task {
     public String titla;
     public boolean done;
 
-    public Task(String titla) {
+    public LocalDate deadLine;
+
+    public Task(String titla, LocalDate deadLine) {
         this.titla = titla;
+        this.deadLine = deadLine;
         this.done = false;
     }
-    public void  makeDone(){
+
+    public void makeDone() {
         this.done = true;
 
     }
+
     @Override
-    public  String toString(){
-        return String.format("%s %s",this.titla,this.done?"СДЕЛАНА":"В ПРОЦЕССЕ");
+    public String toString() {
+        return String.format("%s %s", this.titla, this.done ? "СДЕЛАНА" : "В ПРОЦЕССЕ");
     }
 }
